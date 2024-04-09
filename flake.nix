1916fa7ff357c4d3caae0e609b2ff5fd31d91ca2
pupbrained <mars@pupbrained.xyz>
2024-04-09 02:46:44 -0400

    stuff :D
    Co-authored-by: François-Xavier Talbot <fetchfern@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
         };
 
         # haskell-flake doesn't set the default package, but you can do it here.
-        packages.default = self'.packages.example;
+        packages.default = self'.packages.nix-snow;
       };
     };
 }
--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
-import Relude (getArgs)
+import Data.Version (showVersion)
+import Paths_nix_snow (version)
+import Text.ANSI (bold, underline)
+import Text.InterpolatedString.QM (qmb)
 
 main :: IO ()
 main = do
-    args <- getArgs
-    print args
+  let
+    printHelp =
+      putStrLn
+        [qmb| 
+          {bold $ underline "Usage:"} {bold "nix-snow"} [options] <command>
+
+          {bold $ underline "Commands:"}
+          \  {bold "add"}     Add a package
+          \  {bold "remove"}  Remove a package
+          \  {bold "help"}    Print this help and exit
+
+          {bold $ underline "Options:"}
+          \  {bold "-h, --help"}     Print this help and exit
+          \  {bold "-v, --version"}  Print version information and exit
+        |]
+
+  args <- getArgs
+
+  case () of
+    _
+      | "--help" `elem` args || "-h" `elem` args -> printHelp
+      | "--version" `elem` args || "-v" `elem` args -> putStrLn ("nix-snow " <> showVersion version)
+      | "add" `elem` args -> putStrLn "add"
+      | "remove" `elem` args -> putStrLn "remove"
+      | otherwise -> printHelp
